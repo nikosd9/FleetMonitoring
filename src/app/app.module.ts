28044af58e921927dc9619dd0ebcbdf0fb472abc
Donatos Sarras <donatos.sarras@athrd.prisma.gr>
2018-09-05 14:29:21 +0300

    Map loader service and bing maps loader added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+/// <reference path="../../node_modules/bingmaps/types/MicrosoftMaps/Microsoft.Maps.All.d.ts"/>
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BingMapsLoader} from './services/map-loader.service'
 
-@NgModule({
+@NgModule({ 
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [BingMapsLoader],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/map-loader.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable({providedIn: 'root'})
+export class BingMapsLoader implements IMapLoader {
+  promise:Promise<any>
+
+  load() {
+    if (!this.promise) {
+      this.promise = new Promise((resolve,reject) => {
+        var _window = window as any;
+        _window['MapLoaded'] = (event:any) => resolve('Map Loaded')
+        this.addMapScript();
+      })
+    }
+    return this.promise;
+  }
+
+
+  addMapScript() {
+    var head = document.getElementsByTagName('head')[0];
+
+    var scriptNode = document.createElement('script');
+    scriptNode.type = "text/javascript";
+    scriptNode.src = "https://www.bing.com/api/maps/mapcontrol?key=QSJ56V37iKKbwpEugBDc~Vl9Jy0hBTmdWDsVEeUAadQ~AjEJdhwpQsQqJ19C1DLE5zaww9I1Yl5D3TQuafKinpYoNEFanCI8s_hA-iK_BOHj&callback=MapLoaded"
+  
+    head.appendChild(scriptNode);
+  }
+}
+export interface IMapLoader {
+  load():any
+}
+
+@Injectable({
+  providedIn: 'root',
+  
+})
+export class MapLoaderService {
+  constructor(public mapLoader:BingMapsLoader) { }
+
+  loadMap() {  
+    return this.mapLoader.load();
+  }
+}
+
+
